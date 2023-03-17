4900b725f3a4db2e02d2b7e633c4eecb6b7f4690
Kao <116536454+catfish0102@users.noreply.github.com>
2023-03-17 10:09:33 +0800

    Add files via upload

---
+++ lib/trainers/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .deepedit import DeepEdit
+from .deepgrow import Deepgrow
+from .localization_spine import LocalizationSpine
+from .localization_vertebra import LocalizationVertebra
+from .segmentation import Segmentation
+from .segmentation_spleen import SegmentationSpleen
+from .segmentation_vertebra import SegmentationVertebra
+from .deepedit_heart import DeepEditHeart
+from .segmentation_heart import SegmentationHeart
+from .segmentation_fat import SegmentationFat
+from .segmentation_heart01 import SegmentationHeart01
+from .segmentation_heartaorta import SegmentationHeartAorta
+from .segmentation_cac_class import SegmentationCACClass
+from .segmentation_heaortanet_trial import SegmentationHeaortaNetTrial
+from .segmentation_vatarea import SegmentationVATarea
+from .segmentation_cac_six_class import SegmentationCACsixClass
+from .segmentation_cac_five_class import SegmentationCACfiveClass
